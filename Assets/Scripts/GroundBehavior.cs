e189c1efe41b869f1a9bf087a3670e1396d3479e
Md. Abul Hasan <50300374+iamshantoo@users.noreply.github.com>
2023-02-13 22:22:28 +0600

    added basic functionalities
    player controller
    game manager

---
+++ Assets/Scripts/GroundBehavior.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GroundBehavior : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        float speed = GameManager.Instance.gameSpeed / transform.localScale.x;
+        meshRenderer.material.mainTextureOffset += Vector2.right * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController character;
+    private Vector3 direction;
+
+    public float gravity = 9.81f * 2f;
+    public float jumpForce = 8f;
+
+    private void Awake()
+    {
+        character = GetComponent<CharacterController>();
+    }
+
+    private void OnEnable()
+    {
+        direction = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        direction += Vector3.down * gravity * Time.deltaTime;
+
+        if (character.isGrounded)
+        {
+            direction = Vector3.down;
+
+            if (Input.GetButton("Jump"))
+            {
+                direction = Vector3.up * jumpForce;
+            }
+        }
+
+        character.Move(direction * Time.deltaTime);
+    }
+}
